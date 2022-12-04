95e4dd076dd660083ec07e259c40d98fb80805d5
Tobias <tobias_maier99@gmx.de>
2022-12-04 13:18:37 +0100

    day 4

--- solve04.py
+++ solve04.py
@@ -7,10 +7,20 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
-        "": True,
+            """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""": 2,
     }
     test_data_b = {
-        "": True,
+            """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""": 4,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -32,11 +42,28 @@ def main():
 
 def solve_a(data):
     res = 0
+    for line in data.splitlines():
+        a, b = line.split(",")
+        a0, a1 = [int(x) for x in a.split("-")]
+        b0, b1 = [int(x) for x in b.split("-")]
+        if ((a0 <= b0) and (a1 >= b1)) or ((a0 >= b0) and (a1 <= b1)):
+            res += 1
     return res
 
 
 def solve_b(data):
     res = 0
+    for line in data.splitlines():
+        a, b = line.split(",")
+        a0, a1 = [int(x) for x in a.split("-")]
+        b0, b1 = [int(x) for x in b.split("-")]
+        if (
+            (a0 <= b0 and a1 >= b0) or
+            (a1 >= b1 and a0 <= b1) or
+            (b0 <= a0 and b1 >= a0) or
+            (b1 >= a1 and b0 <= a1)
+        ):
+            res += 1
     return res
 
 

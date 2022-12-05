9858325cc67f71f48220d8f4d118707c66449a1c
Tobias <tobias_maier99@gmx.de>
2022-12-05 06:23:13 +0100

    day 5. FIRST

--- solve05.py
+++ solve05.py
@@ -7,10 +7,26 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
-        "": True,
+        """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+ move 1 from 2 to 1
+ move 3 from 1 to 3
+ move 2 from 2 to 1
+ move 1 from 1 to 2""": "CMZ",
     }
     test_data_b = {
-        "": True,
+        """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+ move 1 from 2 to 1
+ move 3 from 1 to 3
+ move 2 from 2 to 1
+ move 1 from 1 to 2""": "MCD",
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -31,13 +47,47 @@ def main():
 
 
 def solve_a(data):
-    res = 0
-    return res
+    stacks = {}
+    data = data.split("\n\n")
+    stacklines = []
+    for line in data[0].splitlines()[:-1]:
+        s = []
+        for i in range(0, len(line), 4):
+            s.append(line[i:i+4])
+        stacklines.append(s)
+    for stack in range(len(stacklines[0])):
+        stacks[stack] = list(reversed([stackline[stack].strip() for stackline in stacklines if stackline[stack].strip()]))
+
+    for line in data[1].splitlines():
+        sline = line.split()
+        count, _from, _to = int(sline[1]), int(sline[3])-1, int(sline[5])-1
+        for i in range(count):
+            crate = stacks[_from].pop(-1)
+            stacks[_to].append(crate)
+
+    return "".join([stack[-1][1:-1] for stack in stacks.values()])
 
 
 def solve_b(data):
-    res = 0
-    return res
+    stacks = {}
+    data = data.split("\n\n")
+    stacklines = []
+    for line in data[0].splitlines()[:-1]:
+        s = []
+        for i in range(0, len(line), 4):
+            s.append(line[i:i+4])
+        stacklines.append(s)
+    for stack in range(len(stacklines[0])):
+        stacks[stack] = list(reversed([stackline[stack].strip() for stackline in stacklines if stackline[stack].strip()]))
+
+    for line in data[1].splitlines():
+        sline = line.split()
+        count, _from, _to = int(sline[1]), int(sline[3])-1, int(sline[5])-1
+        crates = stacks[_from][-count:]
+        stacks[_to].extend(crates)
+        stacks[_from][-count:] = []
+
+    return "".join([stack[-1][1:-1] for stack in stacks.values()])
 
 
 if __name__ == "__main__":

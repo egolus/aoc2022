1417dd3a6a7222da2d52908df9f39af5ac638b9f
Tobias <tobias_maier99@gmx.de>
2022-12-10 07:26:31 +0100

    day10. part 2 has to be interpreted manually

--- solve10.py
+++ solve10.py
@@ -7,10 +7,300 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
-        "": True,
+        """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop""": 13140,
     }
     test_data_b = {
-        "": True,
+        """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop""": None,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -31,13 +321,59 @@ def main():
 
 
 def solve_a(data):
+    tube = 1
+    cycle = 0
     res = 0
+
+    for line in data.splitlines():
+        if line == "noop":
+            cycle += 1
+        else:
+            val = int(line.split()[1])
+            cycle += 2
+            tube += val
+        if (cycle % 40 == 20) or (cycle % 40 == 21 and line != "noop"):
+            if line == "noop":
+                res += cycle * tube
+                print("noop", cycle * tube)
+            else:
+                if cycle % 20 == 0:
+                    res += cycle * (tube - val)
+                    print("addx", cycle * (tube - val))
+                else:
+                    res += (cycle - 1) * (tube - val)
+                    print("addx last", (cycle - 1) * (tube - val))
+            print(cycle, tube, res)
+        if cycle > 220:
+            break
+
     return res
 
 
 def solve_b(data):
-    res = 0
-    return res
+    tube = 1
+    position = 0
+
+    lines = data.splitlines()
+
+    rest = 0
+    val = 0
+    while lines:
+        for i in range(rest):
+            print("#" if tube - 1 <= position <= tube + 1 else " ", end="")
+            position += 1
+            if position % 40 == 0:
+                position = 0
+                print()
+        tube += val
+        line = lines.pop(0)
+        if line == "noop":
+            rest = 1
+            val = 0
+        else:
+            rest = 2
+            val = int(line.split()[1])
+    print()
 
 
 if __name__ == "__main__":

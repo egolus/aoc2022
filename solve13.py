4181834466de74450a59327589e1d159b537e861
Tobias <tobias_maier99@gmx.de>
2022-12-13 07:15:08 +0100

    day13

--- solve13.py
+++ solve13.py
@@ -1,4 +1,7 @@
+from itertools import zip_longest
+from functools import cmp_to_key
 from aocd import submit, get_data
+from rich import print as pprint
 
 
 def main():
@@ -7,10 +10,54 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
-        "": True,
+        """[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]""": 13,
     }
     test_data_b = {
-        "": True,
+        """[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]""": 140,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -30,14 +77,60 @@ def main():
     submit(result_b, part="b", day=day, year=year)
 
 
+def compare(a, b):
+    if a is None:
+        return True
+    if b is None:
+        return False
+    if isinstance(a, int) and isinstance(b, int):
+        if a < b:
+            return True
+        elif a > b:
+            return False
+        return None
+    elif isinstance(a, str) and isinstance(b, str):
+        return compare(int(a), int(b))
+    elif isinstance(a, (int, str)):
+        return compare([a], b)
+    elif isinstance(b, (int, str)):
+        return compare(a, [b])
+    elif isinstance(a, list) and isinstance(b, list):
+        for ax, bx in zip_longest(a, b):
+            check = compare(ax, bx)
+            if check is None:
+                continue
+            else:
+                return check
+
+
+def compare2(a, b):
+    check = compare(a, b)
+    if check:
+        return -1
+    return 1
+
+
 def solve_a(data):
-    res = 0
-    return res
+    res = []
+    for i, pair in enumerate(data.split("\n\n")):
+        a, b = pair.splitlines()
+        a = eval(a)
+        b = eval(b)
+        if compare(a, b):
+            res.append(i+1)
+
+    return sum(res)
 
 
 def solve_b(data):
-    res = 0
-    return res
+    packets = [[[2]], [[6]]]
+    for line in data.splitlines():
+        if line:
+            packets.append(eval(line))
+
+    packets = sorted(packets, key=cmp_to_key(compare2))
+
+    return (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
 
 
 if __name__ == "__main__":

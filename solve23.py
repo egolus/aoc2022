e7615bbaa4f4fdfe7a774b59b5bb07056e4bf3ef
Tobias <tobias_maier99@gmx.de>
2022-12-23 13:04:35 +0100

    day23

--- solve23.py
+++ solve23.py
@@ -1,4 +1,5 @@
 from aocd import submit, get_data
+from rich import print as pprint
 
 
 def main():
@@ -7,10 +8,24 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
-        "": True,
+"""....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..
+""": 110,
     }
     test_data_b = {
-        "": True,
+"""....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..
+""": 20,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -30,14 +45,139 @@ def main():
     submit(result_b, part="b", day=day, year=year)
 
 
+def drawMap(grid):
+    minx = min(x for (y, x) in grid)
+    maxx = max(x for (y, x) in grid)
+    miny = min(y for (y, x) in grid)
+    maxy = max(y for (y, x) in grid)
+
+    for y in range(miny, maxy+1):
+        for x in range(minx, maxx+1):
+            if (y, x) in grid:
+                print("#", end="")
+            else:
+                print(" ", end="")
+        print()
+    print()
+
+
 def solve_a(data):
+    grid = {}
+
+    moves = [
+        (((-1, -1), (-1, 0), (-1, +1)), (-1, 0)),
+        (((+1, -1), (+1, 0), (+1, +1)), (+1, 0)),
+        (((-1, -1), (0, -1), (+1, -1)), (0, -1)),
+        (((-1, +1), (0, +1), (+1, +1)), (0, +1)),
+     ]
+
+    for y, line in enumerate(data.splitlines()):
+        for x, c in enumerate(line):
+            if c == "#":
+                grid[(y, x)] = c
+
+    # drawMap(grid)
+
+    for round in range(10):
+        for elf in grid:
+            y, x = elf
+            if not any(e in grid for e in (
+                (y-1, x-1),
+                (y-1, x),
+                (y-1, x+1),
+                (y, x-1),
+                (y, x+1),
+                (y+1, x-1),
+                (y+1, x),
+                (y+1, x+1),
+            )):
+                grid[elf] = (y, x)
+            else:
+                for neighbours, move in moves:
+                    if not any((y+e[0], x+e[1]) in grid for e in neighbours):
+                        grid[elf] = (y+move[0], x+move[1])
+                        break
+                else:
+                    grid[elf] = (y, x)
+
+        nextGrid = {}
+        for elf, move in grid.items():
+            if len([m for m in grid.values() if m == move]) == 1:
+                nextGrid[move] = "#"
+            else:
+                nextGrid[elf] = "#"
+
+        moves.append(moves.pop(0))
+        grid = nextGrid
+
+        # drawMap(grid)
+
+    minx = min(x for (y, x) in grid)
+    maxx = max(x for (y, x) in grid)
+    miny = min(y for (y, x) in grid)
+    maxy = max(y for (y, x) in grid)
     res = 0
+    for y in range(miny, maxy+1):
+        for x in range(minx, maxx+1):
+            if (y, x) not in grid:
+                res += 1
     return res
 
 
 def solve_b(data):
-    res = 0
-    return res
+    grid = {}
+
+    moves = [
+        (((-1, -1), (-1, 0), (-1, +1)), (-1, 0)),
+        (((+1, -1), (+1, 0), (+1, +1)), (+1, 0)),
+        (((-1, -1), (0, -1), (+1, -1)), (0, -1)),
+        (((-1, +1), (0, +1), (+1, +1)), (0, +1)),
+     ]
+
+    for y, line in enumerate(data.splitlines()):
+        for x, c in enumerate(line):
+            if c == "#":
+                grid[(y, x)] = c
+
+    # drawMap(grid)
+
+    round = 0
+    while True:
+        round += 1
+        print(round, end="\r")
+        old = tuple(grid)
+        for elf in grid:
+            y, x = elf
+            if not any(e in grid for e in (
+                (y-1, x-1),
+                (y-1, x),
+                (y-1, x+1),
+                (y, x-1),
+                (y, x+1),
+                (y+1, x-1),
+                (y+1, x),
+                (y+1, x+1),
+            )):
+                grid[elf] = (y, x)
+            else:
+                for neighbours, move in moves:
+                    if not any((y+e[0], x+e[1]) in grid for e in neighbours):
+                        grid[elf] = (y+move[0], x+move[1])
+                        break
+                else:
+                    grid[elf] = (y, x)
+
+        nextGrid = {}
+        for elf, move in grid.items():
+            if len([m for m in grid.values() if m == move]) == 1:
+                nextGrid[move] = "#"
+            else:
+                nextGrid[elf] = "#"
+
+        moves.append(moves.pop(0))
+        if tuple(nextGrid) == old:
+            return round
+        grid = nextGrid
 
 
 if __name__ == "__main__":
